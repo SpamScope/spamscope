38ed67f4441e3805d8dde32569410be2c8e7c1f8
Fedele Mantuano <mantuano.fedele@gmail.com>
2016-06-19 11:34:50 +0200

    Fist step phishing bolt. Fixed json_maker possible memory leak with use of dicp pop function. Changed schema topology.

--- src/bolts/phishing.py
+++ src/bolts/phishing.py
@@ -17,40 +17,106 @@ limitations under the License.
 from __future__ import absolute_import, print_function, unicode_literals
 from bolts.abstracts import AbstractBolt
 
-from modules.utils import search_words
-import re
-
 try:
     import simplejson as json
 except ImportError:
     import json
 
+from modules.phishing_bitmap import PhishingBitMap
+
 
 class Phishing(AbstractBolt):
-    # TODO: Handling Tick Tuples reload keywords
 
     def initialize(self, stormconf, context):
         super(Phishing, self).initialize(stormconf, context)
 
-        self.keywords = [
-            "banca marche",
-            "cartasi",
-            "amazon",
-            "dropbox",
-            "aosta",
-        ]
+        # Input bolts for Phishing bolt
+        self.input_bolts = set(
+            [
+                "tokenizer-bolt",
+                "attachments-bolt",
+                "urls_handler_body-bolt",
+            ]
+        )
+
+        # All mails
+        self.mails = {}
+
+        # Load key words
+        self._load_generic_keywords()
+        self._load_subject_keywords()
+
+        # Phishing bitmap
+        self._phishing_bitmap = PhishingBitMap()
+
+    def _load_generic_keywords(self):
+        pass
+
+    def _load_subject_keywords(self):
+        pass
+
+    def _search_phishing(self, greedy_data):
+        # Reset phishing bitmap
+        self._phishing_bitmap.reset_score()
+
+        # Tokenizer
+        mail = json.loads(greedy_data['tokenizer-bolt'][1])
+        body = mail.get('body')
+        subject = mail.get('subject')
+        from_ = mail.get('from')
+
+        # Check body
+        if body:
+            pass
+
+        # Check subject
+        if subject:
+            pass
+
+        # Check from mail
+        if from_:
+            pass
+
+        # Attachments
+        with_attachments = greedy_data['attachments-bolt'][1]
+        if with_attachments:
+            # TODO: if tika
+            pass
+
+        # Urls in body
+        with_urls_body = greedy_data['urls_handler_body-bolt'][1]
+        if with_urls_body:
+            pass
+
+        return self._phishing_bitmap.score
+
+    def process_tick(self, freq):
+        """Every freq seconds you reload the keywords. """
+
+        self._load_generic_keywords()
+        self._load_subject_keywords()
 
     def process(self, tup):
-        sha256_random = tup.values[0]
-        phishing = False
-
         try:
-            mail = json.loads(tup.values[1])
-            words_list = set(re.findall(r"[\w]+", mail.get("body")))
+            bolt = tup.component
+            sha256_random = tup.values[0]
+            values = tup.values
 
-            if search_words(self.keywords, words_list):
-                phishing = True
-                self.log("Phishing for mail '{}'".format(sha256_random))
+            if self.mails.get(sha256_random, None):
+                self.mails[sha256_random][bolt] = values
+            else:
+                self.mails[sha256_random] = {bolt: values}
+
+            diff = self.input_bolts - set(self.mails[sha256_random].keys())
+            if not diff:
+                with_phishing = False
+                score = self._search_phishing(
+                    self.mails.pop(sha256_random)
+                )
+                if score:
+                    with_phishing = True
+
+                self.emit([sha256_random, with_phishing, score])
 
         except Exception as e:
             self.log(
@@ -60,6 +126,3 @@ class Phishing(AbstractBolt):
                 level="error"
             )
             self.raise_exception(e, tup)
-
-        finally:
-            self.emit([sha256_random, phishing])
