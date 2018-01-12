75233e49e2afcaaa8b4403036cd6e3f7fe2ac9c5
Fedele Mantuano <mantuano.fedele@gmail.com>
2018-01-12 02:37:32 +0100

    Fixed Thug integration to work with data stream

--- src/modules/attachments/thug_analysis.py
+++ src/modules/attachments/thug_analysis.py
@@ -43,8 +43,30 @@ import simplejson as json
 log = logging.getLogger("Thug")
 
 
+def generate_json_report():
+    """
+    Return JSON Thug report from logging
+    """
+    if not log.ThugOpts.json_logging:
+        return
+
+    p = log.ThugLogging.modules.get('json', None)
+    if p is None:
+        return
+
+    m = getattr(p, 'get_json_data', None)
+    if m is None:
+        return
+
+    report = json.loads(m(tempfile.gettempdir()))
+    return report
+
+
 class CustomWatchdog(Watchdog):
     def handler(self, signum, frame):
+        """
+        Function that handles Thug timeout
+        """
         msg = "The analysis took more than {} seconds.".format(self.time)
         log.critical(msg)
 
@@ -52,7 +74,7 @@ class CustomWatchdog(Watchdog):
             self.callback(signum, frame)
 
         log.ThugLogging.log_event()
-        raise BaseException(msg)
+        raise Exception(msg)
 
 
 class ThugAnalysis(ThugAPI):
@@ -69,43 +91,40 @@ class ThugAnalysis(ThugAPI):
                 dft = DFT(window)
                 dft.run()
 
-    def generate_json_report(self):
-        if not log.ThugOpts.json_logging:
-            return
-
-        p = log.ThugLogging.modules.get('json', None)
-        if p is None:
-            return
-
-        m = getattr(p, 'get_json_data', None)
-        if m is None:
-            return
-
-        report = json.loads(m(tempfile.gettempdir()))
-        return report
-
     def run(self, attachment, **conf):
         results = []
+
+        # Parameters
+        user_agents = conf.get("user_agents", [])
+        referer = conf.get("referer", "http://www.google.com/")
+        timeout = int(conf.get("timeout", 10))
+        connect_timeout = int(conf.get("connect_timeout", 1))
+        disable_cert_logging = conf.get("disable_cert_logging", True)
+        disable_code_logging = conf.get("disable_code_logging", True)
+        threshold = int(conf.get("threshold", 1))
 
         try:
             local_file = write_payload(
                 attachment["payload"], attachment["extension"],
                 attachment["content_transfer_encoding"])
         except KeyError:
-            # Path
             # If file is in archive it doesn't have content_transfer_encoding
             # keyword. In these cases content_transfer_encoding is base64
             local_file = write_payload(
                 attachment["payload"], attachment["extension"])
 
         # Thug analysis
-        for u in conf["user_agents"]:
-            try:
-                analysis = self.analyze(
-                    local_file, u, conf["referer"], conf["timeout"])
-                results.append(analysis)
-            except UnicodeDecodeError:
-                log.warning("UnicodeDecodeError for in Thug analysis")
+        for u in user_agents:
+            analysis = self.analyze(
+                local_file=local_file,
+                connect_timeout=connect_timeout,
+                disable_cert_logging=disable_cert_logging,
+                disable_code_logging=disable_code_logging,
+                referer=referer,
+                threshold=threshold,
+                timeout=timeout,
+                useragent=u)
+            results.append(analysis)
         else:
             try:
                 os.remove(local_file)
@@ -116,15 +135,25 @@ class ThugAnalysis(ThugAPI):
 
     def analyze(self,
                 local_file,
-                useragent="win7ie90",
+                connect_timeout=2,
+                disable_cert_logging=True,
+                disable_code_logging=True,
                 referer="http://www.google.com/",
-                timeout=5):
+                threshold=1,
+                timeout=10,
+                useragent="win7ie90",
+                ):
         """ It performs the Thug analysis agaist a loca file
 
         Args:
             local_file (string): Local file (on filesystem) to analyze
-            useragent (string): User agent to use for analysis
+            connect_timeout (int): Set the connect timeout
+            disable_cert_logging (bool): Disable SSL/TLS certificate logging
+            disable_code_logging (bool): Disable code logging
             referer (string): Referer to use for analysis
+            threshold (int): Maximum pages to fetch
+            timeout (int): Set the analysis timeout
+            useragent (string): User agent to use for analysis
 
         Returns:
             Returns a Python object with the analysis
@@ -135,8 +164,22 @@ class ThugAnalysis(ThugAPI):
         # Set referer
         self.set_referer(referer)
 
-        # Set timeout
+        # Set the analysis timeout
         self.set_timeout(timeout)
+
+        # Maximum pages to fetch
+        self.set_threshold(threshold)
+
+        # Set the connect timeout
+        self.set_connect_timeout(connect_timeout)
+
+        # Disable code logging
+        if disable_code_logging:
+            self.disable_code_logging()
+
+        # Disable SSL/TLS certificate logging
+        if disable_cert_logging:
+            self.disable_cert_logging()
 
         # No console log
         self.set_log_quiet()
@@ -147,13 +190,13 @@ class ThugAnalysis(ThugAPI):
         # Initialize logging
         self.log_init(local_file)
 
-        # Run analysis
         try:
+            # Run analysis
+            # Analysis can go in timeout, in these cases is handled from
+            # handler function
             self.run_local(local_file)
-        finally:
-            # catch timeout exception to get partial result
 
             # Log analysis results
             self.log_event()
-
-            return self.generate_json_report()
+        finally:
+            return generate_json_report()
